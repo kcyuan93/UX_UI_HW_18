ac82430b9cf7e5613019d0e61119b715ed06b4dc
kcyuan93 <76415009+kcyuan93@users.noreply.github.com>
2021-08-09 00:02:54 -0400

    hover on text show image

--- js/index.js
+++ js/index.js
@@ -1,5 +1,71 @@
-/*$("#toggle").on("click", function(){
-    $("#dropDown").slideToggle();
-  });
-  */
-  
+/*skill1*/
+
+$("#skill1").on("mouseenter", function(){
+  $("#hero1").css("display","block")
+});
+
+$("#skill1").on("mouseleave", function(){
+  $("#hero1").css("display","none")
+});
+
+$("#skill1").on("mouseenter", function(){
+  $("#skill1").css("color","#ca5268")
+});
+
+$("#skill1").on("mouseleave", function(){
+  $("#skill1").css("color","black")
+});
+
+/*skill2*/
+
+$("#skill2").on("mouseenter", function(){
+  $("#hero2").css("display","block")
+});
+
+$("#skill2").on("mouseleave", function(){
+  $("#hero2").css("display","none")
+});
+
+$("#skill2").on("mouseenter", function(){
+  $("#skill2").css("color","#ca5268")
+});
+
+$("#skill2").on("mouseleave", function(){
+  $("#skill2").css("color","black")
+});
+
+/*skill3*/
+
+$("#skill3").on("mouseenter", function(){
+  $("#hero3").css("display","block")
+});
+
+$("#skill3").on("mouseleave", function(){
+  $("#hero3").css("display","none")
+});
+
+$("#skill3").on("mouseenter", function(){
+  $("#skill3").css("color","#ca5268")
+});
+
+$("#skill3").on("mouseleave", function(){
+  $("#skill3").css("color","black")
+});
+
+/*skill4*/
+
+$("#skill4").on("mouseenter", function(){
+  $("#hero4").css("display","block")
+});
+
+$("#skill4").on("mouseleave", function(){
+  $("#hero4").css("display","none")
+});
+
+$("#skill4").on("mouseenter", function(){
+  $("#skill4").css("color","#ca5268")
+});
+
+$("#skill4").on("mouseleave", function(){
+  $("#skill4").css("color","black")
+});
